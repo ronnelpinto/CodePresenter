813a51a2ec237ab410798689ca33125e91c821b1
Ronnel Maclean Pinto <ronnel.pinto@gmail.com>
2015-04-28 12:22:12 +0530

    Added save to new dir functionality
    - saves to new directory for persistence
    - removed temp snippet code

---
+++ move_to_save_dir.php
@@ -0,0 +1,23 @@
+<?php
+if ($handle = opendir('./data/')) {
+    while (false !== ($entry = readdir($handle))) {
+        if ($entry != "." && $entry != "..") {
+		
+		if (strpos($entry,'tar.gz') !== false) {
+			echo "$entry";
+			
+			// this will move it and it won't be present in old location
+			rename("./data/".$entry, "./save_folder/".$entry);
+			
+			// if you want it to be in the new and old location use
+			//copy("./data/".$entry, "./save_folder/".$entry);
+
+		}
+		else{
+			//echo 'false';
+		}
+        }
+    }
+    closedir($handle);
+}
+?>
---
+++ read_directory.php
@@ -0,0 +1,10 @@
+<?php
+if ($handle = opendir('./data/')) {
+    while (false !== ($entry = readdir($handle))) {
+        if ($entry != "." && $entry != "..") {
+            echo "$entry\n";
+        }
+    }
+    closedir($handle);
+}
+?>
