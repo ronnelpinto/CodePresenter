e6126e4d47772699a8d47ff0e72aeec800c53216
Ronnel Maclean Pinto <ronnel_pinto@intuit.com>
2016-10-31 20:23:36 +0530

    Update getImage.php

--- getImage.php
+++ getImage.php
@@ -1,12 +1,6 @@
 <?php
-
 $file = $_GET['file']; // don't forget to sanitize this!
 $img_data = file_get_contents("./data/".$file);
-
 header("Content-type: image/jpeg");
-
 echo $img_data;
-
-
-
 ?>
